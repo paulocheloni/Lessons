6b7de09321242783992e467b65ef0e0e6d83f25b
Paulo <paulocheloni@gmail.com>
2020-06-30 09:35:45 -0300

    ContaArquivos.sh

---
+++ ContaArquivos.sh
@@ -0,0 +1,19 @@
+CAMINHO=$*
+if [ -e $CAMINHO ]
+    then
+        if [ -d $CAMINHO ]
+        then
+            LISTAGEM=`ls $CAMINHO`
+            COUNT=0
+            for ARG in $LISTAGEM;
+            do
+                COUNT=` expr $COUNT + 1`
+            done
+            echo "o diretorio  $CAMINHO tem $COUNT arquivos"
+
+            else 
+                echo "o caminho $CAMINHO nao e diretorio"
+        fi
+else 
+    echo "o caminho $CAMINHO nao existe"
+fi    

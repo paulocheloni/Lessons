b0e560be263d9bdf744e3a5d01f6c72d8b3599c6
Paulo <paulocheloni@gmail.com>
2020-06-30 09:35:16 -0300

    VER_PATH.sh

---
+++ VER_PATH.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+clear
+VAR=$*
+
+if [ -f $VAR ]
+then
+   echo "o caminho  $VAR esta habilitado (arquivo) "
+   ls -l $VAR
+   if [ -w $VAR ]
+       then 
+            echo "voce tem permissao de escrita no arquivo $VAR"
+       else
+    echo "voce nao foi autorizado para alterar o arquivo $VAR"
+    fi  
+elif [ -d $VAR ]
+then
+    echo "o caminho  $VAR e (diretorio) "
+    ls -l $VAR
+    if [ -w $VAR ]
+       then 
+       echo "voce tem permissao de escrita no diretorio contido no caminho $VAR"
+    else
+       echo "voce nao foi autorizado para alterar o diretorio no caminho $VAR"
+    fi
+else 
+    echo "o caminho $VAR nao existe"
+fi
+
+
